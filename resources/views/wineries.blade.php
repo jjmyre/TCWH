e12e560c755f9e0ae1e8cb6e1c6ee29fbbed4507
Justin Myre <justin.myre@gmail.com>
2018-08-26 15:46:22 -0700

    uikit

--- resources/views/wineries.blade.php
+++ resources/views/wineries.blade.php
@@ -4,13 +4,18 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Tri-Cities Wine Hub</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('uikit/css/uikit.css') }}" type='text/css' rel='stylesheet' />
+        <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous">
+        </script>
+
     </head>
     <body>
-        <h1> Tri-Cities Wine Hub Works!
+        <h1 class="uk-heading-line uk-text-center"><span>Tri-Cities Wine Hub</span></h1>
     </body>
 </html>
